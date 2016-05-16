b865802c991dde00f960f209fe13b7e50772fd65
Schuyler Grisham <Schuyler Grisham>
2016-05-16 13:12:24 -0400

    Adding the Final Challenge of the Objects course.

---
+++ DataStructure_Challenge1/js/students.js
@@ -0,0 +1,25 @@
+var students = [
+  {
+    name: 'Dave',
+    track: 'Front End Development',
+    achievements: 158,
+    points: 14730
+  },
+  {
+    name: 'Jody',
+    track: 'iOS Development with Swift',
+    achievements: 175,
+    points: 15830
+  },
+  {
+    name: 'Bob',
+    track: 'iOS Development with Swift',
+    achievements: 130,
+    points: 13040
+  },
+  {
+      name: 'Nick',
+      track: 'iOS Development with Swift',
+      achievements: 190,
+      points: 16350
+  }]
